3239dbc61acde43bb1c508b78fedf1918c1959a7
Cristóbal Paredes <cristobalgutierreznavarro@gmail.com>
2022-11-14 23:11:09 -0300

    2do Commit: Se maqueta en html 1 input para agregar tareas nuevas, 1 botón para agregar las tareas nuevas y se crea funcionalidad en JS.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-console.log("Vinculado");
+const inputTarea = document.querySelector("#inputTarea");
+const agregarTarea = document.querySelector("#agregarTarea");
+const listaTareas = document.querySelector("#listaTareas");
+const cuentaTareas = document.querySelector("#cuenta-tareas");
+const tareas = [];
+let listaId = tareas.length;
+
+agregarTarea.addEventListener("click", () => {
+  const nuevaTarea = inputTarea.value
+  tareas.push(nuevaTarea)
+  inputTarea.value = ""
+
+  let html = ""
+  for (let tarea of tareas){
+    html += `<li>${tarea}</li>`;
+  }
+  listaTareas.innerHTML = html;
+})
